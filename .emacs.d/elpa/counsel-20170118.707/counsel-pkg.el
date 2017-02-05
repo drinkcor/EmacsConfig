24e94c37a7c51c9d5f2347ce6b0bc3138dd60a7e
drinkcor <529971768@qq.com>
2017-02-05 22:13:46 +0800

    remove files in .emacs.d

--- .emacs.d/elpa/counsel-20170118.707/counsel-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "counsel" "20170118.707" "Various completion functions using Ivy" (quote ((emacs "24.3") (swiper "0.8.0"))))
--- .emacs.d/elpa/hungry-delete-20161128.2238/hungry-delete-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "hungry-delete" "20161128.2238" "hungry delete minor mode" (quote nil))
